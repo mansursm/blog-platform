42087b9caaacbcfcd86ecd9c20d3f0a8894cfbe3
Mansur <45202319+mansurmansur@users.noreply.github.com>
2024-05-19 22:16:03 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        compress: true,
+        port: 3000,
+        open: true,
+        hot: true,
+        historyApiFallback: true, // This is used for React Router
+        static: {
+          directory: path.join(__dirname, 'public'), // Serve your static assets from the 'public' directory
+        },
+    },
+};
